8d053bd5a5f50cbc6fed08ed039a6b50f586b974
Tiago Abreu Marques <tiagomarqs09@gmail.com>
2026-05-15 10:23:41 -0300

    Update bintree.h

--- ED2/bintree/bintree.h
+++ ED2/bintree/bintree.h
@@ -2,7 +2,6 @@
 #define BINTREE_H
 #include <stdbool.h>
 
-typedef enum { RED, BLACK } Color;
 typedef struct _no No;
 
 typedef struct _bintree{
@@ -10,17 +9,9 @@ typedef struct _bintree{
 	unsigned int rotacoes;
 }BinTree;
 
-typedef struct _rbtree{
-	No* root;
-	No* nil;
-	unsigned int rotacoes, coloracoes;
-}RBtree;
-
 BinTree* BTcreate();
-RBtree* RBcreate();
 
 bool BTinsert(BinTree*, int);
-bool RBinsert(RBtree*, int);
 bool Tree_delete(BinTree*, No*);
 bool isbst (No*);
 int BTaltura(No*);
@@ -35,10 +26,8 @@ No* BTsearch(No*, int);
 No* BTmax(No*);
 int BTmin(No*);
 No* BTsucc(No*);
-No* lerArvore(char*, int* );
 No* rotacaoEsq(BinTree*, No*);
 No* rotacaoDir(BinTree*, No*);
-void RBinsertFixup(RBtree*, No*);
 int BTnodeKey(No*);
 
 
