bf0309a5b32dc784edc168c4fe726e359777941e
Tiago Abreu Marques <tiagomarqs09@gmail.com>
2026-05-15 10:36:36 -0300

    update rbtree.h

--- ED2/rbtree/rbtree.h
+++ ED2/rbtree/rbtree.h
@@ -1,12 +1,33 @@
-#ifndef RBTREE_H
-#define RBTREE_H
+#ifndef BINTREE_H
+#define BINTREE_H
 #include <stdbool.h>
 
+typedef enum { RED, BLACK } Color;
 typedef struct _no No;
 
-typedef struct _rbtree {
-    
+typedef struct _rbtree{
+	No* root;
+	No* nil;
+	unsigned int rotacoes, coloracoes;
 }RBtree;
+
+RBtree* RBcreate();
+
+bool RBinsert(RBtree*, int);
+int BTaltura(No*);
+short alturaNo(No*);
+void BTpre(No*);
+void BTprint(No*);
+void BTin(No*);
+void BTpos(No*);
+void transplant(BinTree*, No*, No*);
+No* BTsearch(No*, int);
+No* BTmax(No*);
+int BTmin(No*);
+No* BTsucc(No*);
+void RBinsertFixup(RBtree*, No*);
+int BTnodeKey(No*);
+
 
 
 #endif
